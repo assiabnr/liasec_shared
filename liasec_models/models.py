68a724322eb5efc0d1acb3ab6cfaeb76240ced4f
assiabnr <167438436+assiabnr@users.noreply.github.com>
2025-06-16 13:56:35 +0200

    Update models.py

--- liasec_models/models.py
+++ liasec_models/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import PermissionsMixin, Group, Permission
 from django.db import models
 from django.utils import timezone
 
+from django.utils.timezone import now
 
 
 
