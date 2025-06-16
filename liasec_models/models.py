f41907f1c211c7b19e56033d71b1aa414e907eb8
abenrahou <abenrahou05@gmail.com>
2025-06-16 14:02:30 +0200

    ajout migration

--- liasec_models/models.py
+++ liasec_models/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin, Group, Permission
 from django.db import models
 from django.utils import timezone
+from django.utils.timezone import now
 
 
 
